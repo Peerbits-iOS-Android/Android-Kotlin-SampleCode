1673acb09e617b2b11aabb326370a24912fdd8db
ShahnavazAnsariBit <ansari@peerbits.com>
2021-04-08 17:50:39 +0530

    Base Project For Android in Kotlin(MVVM)

---
+++ app/src/main/java/com/peerbits/base/network/API_END_POINTS.kt
@@ -0,0 +1,11 @@
+package com.peerbits.base.network
+
+import com.peerbits.base.BuildConfig
+
+/**
+ * Created by Deep Patel on 1/8/2018
+ */
+
+object API_END_POINTS {
+    const val TOKEN = "auths/connect/token"
+}
